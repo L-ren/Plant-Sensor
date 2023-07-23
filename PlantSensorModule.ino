66a9f24def428d38438e18a9a464cb40096e14b5
Loren <loren.johnson99@yahoo.com>
2023-07-23 12:50:04 -0700

    reads from DHT22 humidity/temp sensor

--- PlantSensorModule.ino
+++ PlantSensorModule.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_VEML7700.h>
+#include <DHT_U.h>
+#include <DHT.h>
 
+Adafruit_VEML7700 veml = Adafruit_VEML7700();
+
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(8, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+void setup() {
+  Serial.begin(9600); //A0
+  // Serial1.begin(115200); // baud rate for lux sensor
+
+  // set up light sensor
+  // while (!Serial) { 
+  //   delay(10); 
+  // }
+  // Serial.println("Adafruit VEML7700 Test");
+  // if (!veml.begin()) {
+  //  Serial.println("Sensor not found");
+  //  while (1);
+  // }
+  // Serial.println("Sensor found");
+  // veml.setLowThreshold(10000);
+  // veml.setHighThreshold(20000);
+  // veml.interruptEnable(true);
+
+  // set up dht and soil sensor
+  dht.begin();
+  delay(100);
+
+  Serial.println("Setup Success!"); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(1000);
+  Serial.println(dht.readHumidity());
 
 }
